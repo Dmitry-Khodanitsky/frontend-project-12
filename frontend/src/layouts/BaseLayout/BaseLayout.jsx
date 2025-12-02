804cc99cd732cacd5138133a0dfffff4a2c91780
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-02 14:30:45 +0300

    feat: добавить базовый layout который будет использоваться всеми экранами

---
+++ frontend/src/layouts/BaseLayout/BaseLayout.jsx
@@ -0,0 +1,15 @@
+import { Container } from 'react-bootstrap'
+
+const BaseLayout = ({ children }) => {
+  return (
+    <Container
+      fluid
+      className="min-vh-100 d-flex flex-column justify-content-center align-items-center p-3"
+    >
+      <div className="col-12 col-md-8 col-xxl-6">
+        {children}
+      </div>
+    </Container>
+  )
+}
+export default BaseLayout
