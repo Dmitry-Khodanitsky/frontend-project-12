1fb967ea21b9afd8c80a5733715fa40d32672176
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-04 17:14:28 +0300

    refactor: улучшить сетку лэйаута с учетом добавления header

--- frontend/src/layouts/BaseLayout/BaseLayout.jsx
+++ frontend/src/layouts/BaseLayout/BaseLayout.jsx
@@ -1,4 +1,6 @@
 import { Container } from 'react-bootstrap'
+import Header from '../../components/Header/Header'
+import { useEffect } from 'react'
 
 const BaseLayout = ({ children }) => {
   useEffect(() => {
@@ -6,12 +8,10 @@ const BaseLayout = ({ children }) => {
   }, [])
 
   return (
-    <Container
-      fluid
-      className="min-vh-100 d-flex flex-column justify-content-center align-items-center p-3"
-    >
-      <div className="col-12 col-md-8 col-xxl-6">
-        {children}
+    <Container fluid className="min-vh-100 d-flex flex-column text-light p-0">
+      <Header />
+      <div className="flex-grow-1 d-flex flex-column justify-content-center align-items-center">
+        <div className="col-12 d-flex align-items-center justify-content-center">{children}</div>
       </div>
     </Container>
   )
