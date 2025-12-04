6e6bf79a911a3dbc949212228b184d85d4a00863
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-04 15:49:05 +0300

    feat: добавить слайс с авторизацией

---
+++ frontend/src/store/authSlice.js
@@ -0,0 +1,57 @@
+import { createSlice, createAsyncThunk, current } from '@reduxjs/toolkit'
+import axios from 'axios'
+
+export const fetchAuthToken = createAsyncThunk(
+  'auth/logIn',
+  async ({ username, password }, { rejectWithValue }) => {
+    try {
+      const response = await axios.post('/api/v1/login', {
+        username,
+        password,
+      })
+      console.log(response.data) // удалить в проде
+      return response.data
+    } catch (error) {
+      return rejectWithValue(error.response.statusText)
+    }
+  }
+)
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: {
+    user: null,
+    token: localStorage.getItem('token'),
+    error: null,
+    isLoading: false,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAuthToken.pending, (state) => {
+        state.error = null
+        state.isLoading = true
+      })
+      .addCase(fetchAuthToken.rejected, (state, action) => {
+        state.error = action.payload
+        state.isLoading = false
+        console.log(current(state))
+      })
+      .addCase(fetchAuthToken.fulfilled, (state, action) => {
+        const { user, token } = action.payload
+        state.error = null
+        state.user = user
+        state.token = token
+        state.isLoading = false
+        if (token) {
+          localStorage.setItem('token', token)
+        }
+      })
+    // добавить pending
+  },
+})
+
+export default authSlice.reducer
+
+export const selectToken = (state) => state.auth.token
+export const selectError = (state) => state.auth.error
