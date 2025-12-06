4d313c73f955e5b76b42feadeec9f9cbbca01716
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-06 12:57:16 +0300

    feat: добавить в компонент MainScreen список каналов  и секцию с сообщениями

--- frontend/src/screens/MainScreen/MainScreen.jsx
+++ frontend/src/screens/MainScreen/MainScreen.jsx
@@ -2,6 +2,10 @@ import { useSelector } from 'react-redux'
 import { Navigate } from 'react-router'
 import { selectToken } from '../../store/authSlice'
 
+import ChannelsList from '../../components/ChannelsList/ChannelsList'
+import ConversationSection from '../../components/ChatsList/ConversationSection'
+import { Container, Row, Col } from 'react-bootstrap'
+
 const MainScreen = () => {
   const token = useSelector(selectToken)
 
@@ -9,7 +13,14 @@ const MainScreen = () => {
     return <Navigate to="/login" state={{ from: location.pathname }} replace />
   }
 
-  return <h2>Main Screen</h2>
+  return (
+    <Container fluid className="vh-100 p-0">
+      <div className="d-flex h-100">
+        <ChannelsList />
+        <ConversationSection />
+      </div>
+    </Container>
+  )
 }
 
 export default MainScreen
