6965f6805fe77424a12c2db6586b11a934a4534a
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-06 12:57:56 +0300

    feat: создать тайтл для списка каналов и секции с чатом как отдельный компонент

---
+++ frontend/src/components/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,11 @@
+import { Button } from 'react-bootstrap'
+const SectionTitle = ({ name, isEditable }) => {
+  return (
+    <div className="d-flex justify-content-between align-items-center bg-dark-subtle p-2">
+      <b className='p-2'>{name}</b>
+      {isEditable && <Button type='button' variant="outline-light" size='sm'><span>＋</span></Button>}
+    </div>
+  )
+}
+
+export default SectionTitle
