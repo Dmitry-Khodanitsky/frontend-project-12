809ccd6ce11779b452587558c147b39413b3c832
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-06 12:59:41 +0300

    feat: создать компонент  элемент списка каналов

---
+++ frontend/src/components/ChannelsList/ChannelItem.jsx
@@ -0,0 +1,16 @@
+import { Button } from 'react-bootstrap'
+
+const ChanelItem = ({ name }) => {
+  return (
+    <Button
+      type="button"
+      variant="dark"
+      className="w-100 rounded-0 text-start"
+    >
+      <span className="me-1">#</span>
+      {name}
+    </Button>
+  )
+}
+
+export default ChanelItem
