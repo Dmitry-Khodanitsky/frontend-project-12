42f897c1c30a9f0b1eabeecbc8c4bbe31eab4120
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-06 12:55:42 +0300

    feat: добавить слайс для работы с сообщениями

---
+++ frontend/src/store/messagesSlice.js
@@ -0,0 +1,50 @@
+import { createSlice, createAsyncThunk, current } from '@reduxjs/toolkit'
+import axios from 'axios'
+
+export const fetchMessages = createAsyncThunk(
+  'messages/getMessages',
+  async (token, { rejectWithValue }) => {
+    try {
+      const response = await axios.get('/api/v1/messages', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      return response.data
+    } catch (error) {
+      return rejectWithValue(error.response.statusText)
+    }
+  }
+)
+
+const messagesSlice = createSlice({
+  name: 'messages',
+  initialState: {
+    messages: [
+      { id: '1', body: 'text message', channelId: '1', username: 'admin ' },
+    ],
+    error: null,
+    isLoading: false,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchMessages.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(fetchMessages.rejected, (state, action) => {
+        state.error = action.payload
+        state.isLoading = false
+      })
+      .addCase(fetchMessages.fulfilled, (state, action) => {
+        //state.messages = action.payload
+        state.isLoading = false
+        console.log('Fulfiled messages: ', current(state))
+      })
+  },
+})
+
+export default messagesSlice.reducer
+
+export const selectMessages = (state) => state.messages.messages
+export const selectError = (state) => state.messages.error
