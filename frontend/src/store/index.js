ea01d4c9758468f215a83a0832db311e8144b0cc
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-06 12:56:00 +0300

    feat: добаить в стор слайс для работы с сообщениями

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from './authSlice'
+import channelsSlice from './channelsSlice'
+import messagesSlice from './messagesSlice'
 
 const store = configureStore({
   reducer: {
     // Свойство auth будет внутри объекта общего состояния: state.autj
     auth: authReducer,
+    channels: channelsSlice,
+    messages: messagesSlice,
   },
 })
 
