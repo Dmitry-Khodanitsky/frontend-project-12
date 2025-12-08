aacf7ac5057162dcc5cfa33e732bc6894bd1ae05
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-08 16:31:19 +0300

    lint: исправления линтера

--- frontend/src/components/ChannelsList/ChannelItem.jsx
+++ frontend/src/components/ChannelsList/ChannelItem.jsx
@@ -2,11 +2,7 @@ import { Button } from 'react-bootstrap'
 
 const ChanelItem = ({ name }) => {
   return (
-    <Button
-      type="button"
-      variant="dark"
-      className="w-100 rounded-0 text-start"
-    >
+    <Button type="button" variant="dark" className="w-100 rounded-0 text-start">
       <span className="me-1">#</span>
       {name}
     </Button>
