a63c17a47d41eb90cbecb9787bcaf4c79adfd106
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-09 09:55:54 +0300

    refactor: вынести провайдерв отдельный компонент

--- frontend/src/context/channelContext.jsx
+++ frontend/src/context/channelContext.jsx
@@ -1,21 +1,4 @@
 import React from 'react'
-import { useState, useMemo } from 'react'
+
 
 export const ActiveChannelIdContext = React.createContext()
-
-export const ActiveChannelIdProvider = ({ children }) => {
-  const [activeChannelId, setActiveChannelId] = useState('1')
-
-  const providerValue = useMemo(
-    () => ({
-      activeChannelId,
-      setActiveChannelId,
-    }),
-    [activeChannelId]
-  )
-  return (
-    <ActiveChannelIdContext.Provider value={providerValue}>
-      {children}
-    </ActiveChannelIdContext.Provider>
-  )
-}
