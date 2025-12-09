3278b80e7608293db6867a1df3a4d4fe5b23dd2c
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-09 09:57:35 +0300

    refactor: передать в SectionTitle имя канала используя данные из контекста

--- frontend/src/components/ConversationSection/ConversationSection.jsx
+++ frontend/src/components/ConversationSection/ConversationSection.jsx
@@ -1,25 +1,36 @@
 import SectionTitle from '../SectionTitle/SectionTitle'
 import { useDispatch, useSelector } from 'react-redux'
 import { selectToken } from '../../store/authSlice'
-import { useEffect } from 'react'
+import { useEffect, useContext } from 'react'
 import { Tab } from 'react-bootstrap'
 import MessagesList from './MessagesList'
 import { fetchMessages } from '../../store/messagesSlice'
+import { ActiveChannelIdContext } from '../../context/channelContext'
+import { selectChannelById } from '../../store/channelsSlice'
 
 const ConversationSection = () => {
   const dispatch = useDispatch()
   const token = useSelector(selectToken)
+  const { activeChannelId } = useContext(ActiveChannelIdContext)
+  const selectedChannel = useSelector(selectChannelById(activeChannelId))
+
+  const channelName = selectedChannel?.removable
+    ? selectedChannel?.name
+    : `# ${selectedChannel?.name}`
 
   useEffect(() => {
     dispatch(fetchMessages(token))
-  }, [])
+  }, [token])
 
   return (
     <section className="w-100">
-      <SectionTitle name="name" isEditable={false} />
-      <div className='p-3'>
+      <SectionTitle
+        name={selectedChannel ? channelName : 'Выберите канал'}
+        isEditable={false}
+      />
+      <div className="p-3">
         <Tab.Content>
-          <MessagesList />
+          <MessagesList channelId={activeChannelId} />
         </Tab.Content>
       </div>
     </section>
