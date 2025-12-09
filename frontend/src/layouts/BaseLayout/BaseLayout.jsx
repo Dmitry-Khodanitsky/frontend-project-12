935c4051d20682d478d442bf89e8e5d393c66e06
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-09 09:45:55 +0300

    refactor: удалить лишний div который не влияет на сетку

--- frontend/src/layouts/BaseLayout/BaseLayout.jsx
+++ frontend/src/layouts/BaseLayout/BaseLayout.jsx
@@ -6,12 +6,13 @@ const BaseLayout = ({ children }) => {
   useEffect(() => {
     document.documentElement.setAttribute('data-bs-theme', 'dark')
   }, [])
-
   return (
     <Container fluid className="min-vh-100 d-flex flex-column text-light p-0">
       <Header />
-      <div className="flex-grow-1 d-flex flex-column justify-content-center align-items-center">
-        <div className="col-12 d-flex align-items-center justify-content-center">{children}</div>
+
+        <div className="col-12 d-flex align-items-center justify-content-center">
+          {children}
+        
       </div>
     </Container>
   )
