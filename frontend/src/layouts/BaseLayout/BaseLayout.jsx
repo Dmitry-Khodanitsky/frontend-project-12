80dc6cc70b6d122ae5516a5deb7893f30990f441
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-09 11:31:45 +0300

    fix: центрировать содержимое layout

--- frontend/src/layouts/BaseLayout/BaseLayout.jsx
+++ frontend/src/layouts/BaseLayout/BaseLayout.jsx
@@ -9,10 +9,10 @@ const BaseLayout = ({ children }) => {
   return (
     <Container fluid className="min-vh-100 d-flex flex-column text-light p-0">
       <Header />
-
+      <div className="flex-grow-1 d-flex flex-column justify-content-center align-items-center">
         <div className="col-12 d-flex align-items-center justify-content-center">
           {children}
-        
+        </div>
       </div>
     </Container>
   )
