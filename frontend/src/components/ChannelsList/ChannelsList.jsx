af3f8388fdd1e83239e22c76600b29bf2bdf1e22
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-10 11:34:25 +0300

    feat: добавить состояние загрузки каналов
    -Добавлен импорт selectLoading из слайса каналов для получения состояния.
    -В компонент добавлена переменная isLoading, которая отслеживает процесс загрузки каналов.
    -Секция с каналами   теперь отображает компонент Loader r во время загрузки каналов вместо списка каналов.

--- frontend/src/components/ChannelsList/ChannelsList.jsx
+++ frontend/src/components/ChannelsList/ChannelsList.jsx
@@ -1,16 +1,22 @@
 import { useEffect } from 'react'
 import { Nav } from 'react-bootstrap'
 import { useSelector, useDispatch } from 'react-redux'
-import { selectChannels, fetchChannels} from '../../store/channelsSlice'
+import {
+  selectChannels,
+  fetchChannels,
+  selectLoading,
+} from '../../store/channelsSlice'
 
 import { selectToken } from '../../store/authSlice'
 import SectionTitle from '../SectionTitle/SectionTitle'
 import ChannelItem from './ChannelItem'
+import Loader from '../Loader/Loader'
 
 const ChannelsList = () => {
   const channels = useSelector(selectChannels)
   const token = useSelector(selectToken)
   const dispatch = useDispatch()
+  const isLoading = useSelector(selectLoading)
 
   useEffect(() => {
     dispatch(fetchChannels(token))
@@ -25,15 +31,19 @@ const ChannelsList = () => {
       }}
     >
       <SectionTitle name="Каналы" isEditable={true} />
-      <Nav variant="pills" className="flex-column">
-        {channels.map((channel) => {
-          return (
-            <ChannelItem key={channel.id} id={channel.id}>
-              {channel.removable ? channel.name : `# ${channel.name}`}
-            </ChannelItem>
-          )
-        })}
-      </Nav>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <Nav variant="pills" className="flex-column">
+          {channels.map((channel) => {
+            return (
+              <ChannelItem key={channel.id} id={channel.id}>
+                {channel.removable ? channel.name : `# ${channel.name}`}
+              </ChannelItem>
+            )
+          })}
+        </Nav>
+      )}
     </aside>
   )
 }
