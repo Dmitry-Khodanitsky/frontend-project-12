51938ff2b2c129c487007374322b0019d92788e1
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-10 11:22:23 +0300

    feat: создать компонент индикатор загрузки

---
+++ frontend/src/components/Loader/Loader.jsx
@@ -0,0 +1,12 @@
+import LoadingSpinner from '../LoadingSpinner/LoadingSpinner'
+
+const Loader = () => {
+  return (
+    <div className="p-3">
+      Загрузка...
+      <LoadingSpinner />
+    </div>
+  )
+}
+
+export default Loader
