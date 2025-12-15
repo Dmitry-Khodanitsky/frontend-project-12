e94f1f177b0045fec03b3765b944e710181417b8
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-15 16:17:53 +0300

    refactor: удалить неиспользуемые слайсы  в store

--- frontend/src/app/model/store.js
+++ frontend/src/app/model/store.js
@@ -1,16 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { baseApi } from '@/app/api/baseApi'
 import authReducer from '@/features/auth/model/authSlice'
-import channelsSlice from '@/store/channelsSlice'
-import messagesSlice from '@/store/messagesSlice'
 
 const store = configureStore({
   reducer: {
     [baseApi.reducerPath]: baseApi.reducer,
     // Свойство auth будет внутри объекта общего состояния: state.autj
     auth: authReducer,
-    channels: channelsSlice,
-    messages: messagesSlice,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(baseApi.middleware),
