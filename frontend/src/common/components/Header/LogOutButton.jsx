e545d2dcfb6a1e0080df2ae7c42f47f2fa2f04ce
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-15 16:17:30 +0300

    refactor: изменить импорт функции logOut на актуальный

--- frontend/src/common/components/Header/LogOutButton.jsx
+++ frontend/src/common/components/Header/LogOutButton.jsx
@@ -1,6 +1,6 @@
 import { Button } from 'react-bootstrap'
 import { useDispatch } from 'react-redux'
-import { logOut } from '@/store/authSlice'
+import { logOut } from '@/features/auth/model/authSlice'
 export const LogOutButton = () => {
   const dispatch = useDispatch()
   const handleLogout = () => {
