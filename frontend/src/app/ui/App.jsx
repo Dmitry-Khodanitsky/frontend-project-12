78f2144fea88abafd4145e34f61e3a8fa1235ab8
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-15 11:48:43 +0300

    refactor: поместить app компонент в поддиректорию ui

---
+++ frontend/src/app/ui/App.jsx
@@ -0,0 +1,12 @@
+import BaseLayout from '@/common/layouts/BaseLayout'
+import Routing from '@/common/routing/Routing'
+
+function App() {
+  return (
+    <BaseLayout>
+      <Routing />
+    </BaseLayout>
+  )
+}
+
+export default App
