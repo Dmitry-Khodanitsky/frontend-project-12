b3a6708581807326d4dcb7dcf198315b45ce24d5
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-15 16:13:33 +0300

    refactor: добавить обработку пропсов по умолчанию в компоненте сообщения - Добавлены значения по умолчанию для пропсов username и text через деструктуризацию - Добавлена проверка существования username перед получением первой буквы - Введена переменная userNameFirstLetter с проверкой на пустое имя - Использован символ '?' как значение по умолчанию для первой буквы при отсутствии имени - Улучшена надежность компонента при получении неполных или некорректных данных

--- frontend/src/features/messages/ui/MessageItem.jsx
+++ frontend/src/features/messages/ui/MessageItem.jsx
@@ -1,11 +1,12 @@
-const MessageItem = ({ username, text }) => {
+const MessageItem = ({ username = '', text = '' }) => {
+  const userNameFirstLetter = username ? username[0] : '?'
   return (
     <div className="d-flex mb-3">
       <div
         className="d-flex flex-shrink-0 rounded-5 border border-2 border-white me-2 align-items-center justify-content-center"
         style={{ width: 40, height: 40 }}
       >
-        <b className="fs-4">{username[0]}</b>
+        <b className="fs-4">{userNameFirstLetter}</b>
       </div>
       <div className="d-flex flex-column flex-grow-1">
         <b className="text-break">{username}</b>
