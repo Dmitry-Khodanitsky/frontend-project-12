50f4b841ec9ac448c21ad8bbd694c14b815afd49
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-15 11:49:09 +0300

    refactor: отредактировать импорты

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
-import store from './store/index.js'
-import App from '@/app/App.jsx'
+import store from '@/app/model/store'
+import App from '@/app/ui/App'
 import { BrowserRouter } from 'react-router'
 import 'bootstrap/dist/css/bootstrap.min.css'
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
