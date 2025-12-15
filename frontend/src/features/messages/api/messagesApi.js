1d4eb7deb5eeaaab717ab57a4bfc96c2e410336c
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-15 16:49:32 +0300

    feat: добавить подписку на socket при получении новых сообщений в RTK

--- frontend/src/features/messages/api/messagesApi.js
+++ frontend/src/features/messages/api/messagesApi.js
@@ -1,17 +1,47 @@
 import { baseApi } from '@/app/api/baseApi'
+import { socket } from '@/common/services/socket'
 
 export const channelsApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     getMessages: builder.query({
       query: () => 'messages',
+      async onCacheEntryAdded(
+        arg,
+        { updateCachedData, cacheDataLoaded, cacheEntryRemoved }
+      ) {
+        try {
+          // Сначала ждем завершения обработки первоначального запроса при монтировании компонента
+          await cacheDataLoaded
+          console.log('Соединение установлено', socket.id)
+
+          // Когда данные получены из соединения с сервером,
+          //Обновляем результат запроса полученным сообщением
+          const handleNewMessage = (payload) => {
+            //Просто добавляем новое сообщение в существующий массив в кэше
+            // RTK Query сам уведомит компоненты об изменениях
+            updateCachedData((draft) => {
+              draft.push(payload)
+            })
+          }
+          socket.on('newMessage', handleNewMessage)
+        } catch (error) {
+          // Если HTTP запрос завершился ошибкой, подписка не создастся
+
+          console.log(error)
+        }
+        // Когда компонент с чатом размонтируется (или данные станут не нужны),
+        // RTK Query автоматически выполнит этот код:
+        await cacheEntryRemoved
+        socket.off('newMessage')
+      },
     }),
     sendMessage: builder.mutation({
-        query: (message) => ({
-            url: 'messages',
-            method: 'POST',
-            body: message
-        })
-    })
+      query: (message) => ({
+        url: 'messages',
+        method: 'POST',
+        body: message,
+      }),
+    }),
   }),
 })
 
