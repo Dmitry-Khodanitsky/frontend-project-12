3f734ff330a34cc652d8e4c03c4e83ceeda7e9f6
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-16 13:45:50 +0300

    feat: добавить пропс ref для автоматического скролла к активному каналу

--- frontend/src/features/channels/ui/ChannelItem.jsx
+++ frontend/src/features/channels/ui/ChannelItem.jsx
@@ -1,12 +1,12 @@
 import { Nav } from 'react-bootstrap'
-const ChannelItem = ({ id, children }) => {
+const ChannelItem = ({ id, ref, children }) => {
   return (
     <Nav.Item>
       <Nav.Link
+        ref={ref}
         eventKey={id}
         className="rounded-0 text-dark-emphasis"
         style={{ '--bs-nav-pills-link-active-bg': '#1a1d20' }}
-        
       >
         {children}
       </Nav.Link>
