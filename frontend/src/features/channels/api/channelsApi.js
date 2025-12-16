09da8633501152f0220614a6ab09acdfedf2a1cb
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-16 12:11:38 +0300

    feat: добавить socket для добавления каналов

--- frontend/src/features/channels/api/channelsApi.js
+++ frontend/src/features/channels/api/channelsApi.js
@@ -1,11 +1,63 @@
 import { baseApi } from '@/app/api/baseApi'
+import { socket } from '@/common/services/socket'
 
 export const channelsApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     getChannels: builder.query({
       query: () => 'channels',
+      async onCacheEntryAdded(
+        arg,
+        { updateCachedData, cacheDataLoaded, cacheEntryRemoved }
+      ) {
+        try {
+          // Сначала ждем завершения обработки первоначального запроса при монтировании компонента
+          await cacheDataLoaded
+          console.log('Соединение с каналами установлено', socket.id)
+
+          // Когда данные получены из соединения с сервером,
+          //Обновляем результат запроса полученным сообщением
+          const handleNewChannel = (payload) => {
+            //Просто добавляем новое сообщение в существующий массив в кэше
+            // RTK Query сам уведомит компоненты об изменениях
+            updateCachedData((draft) => {
+              draft.push(payload)
+            })
+          }
+
+          const handleRenameChannel = (payload) => {
+            updateCachedData((draft) => {
+              draft.push(payload)
+            })
+          }
+
+          const handleRemoveChannel = (payload) => {
+            updateCachedData((draft) => {
+              draft.filter((draft) => draft.id !== payload)
+            })
+          }
+          socket.on('newChannel', handleNewChannel)
+          socket.on('renameChannel', handleRenameChannel) //слушатель переименовывания канала
+          socket.on('removeChannel', handleRemoveChannel) //слушатель удаления канала
+        } catch (error) {
+          // Если HTTP запрос завершился ошибкой, подписка не создастся
+          console.log(error)
+        }
+        // Когда компонент с чатом размонтируется (или данные станут не нужны),
+        // RTK Query автоматически выполнит этот код:
+        await cacheEntryRemoved
+        socket.off('newChannel')
+        socket.off('renameChannel')
+        socket.off('removeChannel')
+      },
+    }),
+    addChannel: builder.mutation({
+      query: (channel) => ({
+        url: 'channels',
+        method: 'POST',
+        body: channel,
+      }),
     }),
   }),
 })
 
-export const { useGetChannelsQuery } = channelsApi
+export const { useGetChannelsQuery, useAddChannelMutation } = channelsApi
