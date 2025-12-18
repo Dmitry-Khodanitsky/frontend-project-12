67663137d722f30ef6550e155b924193f36807d2
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-18 15:47:09 +0300

    feat: добавить проверку на наличие выбранного канала

--- frontend/src/features/messages/ui/MessagesSection.jsx
+++ frontend/src/features/messages/ui/MessagesSection.jsx
@@ -9,7 +9,7 @@ export const MessagesSection = ({ activeChannelId }) => {
   return (
     <div className="d-flex flex-column w-100 bg-dark-subtle ">
       <div className="p-3 shadow">
-        <b>{`# ${selectedChannel?.name}`}</b>
+        {selectedChannel && <b>{`# ${selectedChannel.name}`}</b>}
       </div>
 
       <div className="flex-grow-1 overflow-hidden">
