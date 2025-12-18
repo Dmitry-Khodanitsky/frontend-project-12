df8039b19598de8f5e7b33c96ca6eb5e76f8eb52
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-18 15:15:08 +0300

    refactor: изменить название пропса toast на notifications для лучшей читаемости

--- frontend/src/common/components/NotificationToast/NotificationToast.jsx
+++ frontend/src/common/components/NotificationToast/NotificationToast.jsx
@@ -2,21 +2,25 @@ import Toast from 'react-bootstrap/Toast'
 import ToastContainer from 'react-bootstrap/ToastContainer'
 import ReactDom from 'react-dom'
 
-export const NotificationToast = ({ toasts, onRemove, from = 'СВЯЗЬ 🤙' }) => {
+export const NotificationToast = ({
+  notifications,
+  onRemove,
+  from = 'СВЯЗЬ 🤙',
+}) => {
   return (
     <ToastContainer
       position="bottom-end"
       className="p-3"
       style={{ zIndex: 2000 }}
     >
-      {toasts.map((toast) => (
+      {notifications.map((notification) => (
         <Toast
-          key={toast.id}
+          key={notification.id}
           show
           delay={5000}
           autohide
-          onClose={() => onRemove(toast.id)}
-          bg={toast.variant}
+          onClose={() => onRemove(notification.id)}
+          bg={notification.variant}
           className="mb-2"
         >
           <Toast.Header>
@@ -24,7 +28,7 @@ export const NotificationToast = ({ toasts, onRemove, from = 'СВЯЗЬ 🤙' }
             {/* сделать показ времени ago */}
             <small>11 mins ago</small>
           </Toast.Header>
-          <Toast.Body>{toast.message}</Toast.Body>
+          <Toast.Body>{notification.message}</Toast.Body>
         </Toast>
       ))}
     </ToastContainer>
