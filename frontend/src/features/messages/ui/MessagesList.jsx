37bdf1dfe60312660f44945870c9d92f03e000a6
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-18 15:51:14 +0300

    feat: сделать обработку ошибок если сообщения и каналы не загрузились

--- frontend/src/features/messages/ui/MessagesList.jsx
+++ frontend/src/features/messages/ui/MessagesList.jsx
@@ -1,25 +1,34 @@
-import { useEffect, useRef } from 'react'
 import MessageItem from './MessageItem'
 import { Loader } from '@/common/components/'
 import { useChannelMessages, useScrollTo } from '@/common/hooks'
+import { useGetChannelsQuery } from '@/features/channels/api/channelsApi'
+import { useNotification } from '@/app/model/NotifyContext'
+import { useEffect } from 'react'
 
 const MessagesList = ({ channelId }) => {
-  const { channelMessages, error, isLoading } = useChannelMessages(channelId)
+  const { error: channelsError } = useGetChannelsQuery()
+  const {
+    channelMessages,
+    error: messagesError,
+    isLoading,
+  } = useChannelMessages(channelId)
+  const showNotification = useNotification()
 
   const lastMessageRef = useScrollTo(channelMessages, 'auto')
 
-  if (!channelId) return <h4>Кажется мы не смогли найти такой канал</h4>
-
-  if (error) {
-    return <h4>Что-то пошло не так</h4>
-  }
+  useEffect(() => {
+    if (channelsError) return
+    if (messagesError) {
+      showNotification('Не получилось загрузить сообщения', 'danger')
+    }
+  }, [channelsError, messagesError])
 
   return (
     <div style={{ minHeight: '100%' }}>
       {isLoading ? (
         <Loader />
       ) : (
-        channelMessages.map((message) => (
+        channelMessages?.map((message) => (
           <MessageItem
             key={message.id}
             username={message.username}
