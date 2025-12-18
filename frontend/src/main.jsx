a12c89206698288696e0908a8a86d1450857f408
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-18 15:15:27 +0300

    feat: обернуть приложение в провайдер уведомлений

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,16 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import store from '@/app/model/store'
+import { NotifyProvider } from './app/model/NotifyContext'
 import App from '@/app/ui/App'
 import { BrowserRouter } from 'react-router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Provider store={store}>
+  //<StrictMode>
+  <Provider store={store}>
+    <NotifyProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </Provider>
-  </StrictMode>
+    </NotifyProvider>
+  </Provider>
+  //</StrictMode>
 )
