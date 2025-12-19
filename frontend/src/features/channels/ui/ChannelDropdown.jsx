715253e71f199ccfa24378ada4843dfe4370ab3f
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-19 16:30:42 +0300

    feat: добавить компонент дропдаун -Добавлено контекстное меню с кнопками для удаления и редактирования каналов

---
+++ frontend/src/features/channels/ui/ChannelDropdown.jsx
@@ -0,0 +1,61 @@
+import { Dropdown } from 'react-bootstrap'
+import { useState, useContext } from 'react'
+import { useRemoveChannelMutation } from '../api/channelsApi'
+import { useNotification } from '@/app/model/NotifyContext'
+import { ModalWrapper, ModalButtons } from '@/common/components'
+import { ActiveChannelIdContext } from '@/pages/MainPage/model'
+
+export const ChannelDropdown = ({ id }) => {
+  const [visible, setVisible] = useState(false)
+  const [removeChannel, { isLoading }] = useRemoveChannelMutation()
+  const { activeChannelId, setActiveChannelId } = useContext(
+    ActiveChannelIdContext
+  )
+  const showNotification = useNotification()
+  const handleOpen = () => setVisible(true)
+  const handleClose = () => setVisible(false)
+
+  const handleRemove = async () => {
+    try {
+      await removeChannel(id).unwrap()
+      //удалить сообщения этого канала
+      // сделать активным канал general если пользователи были в удаленном канале
+      if (activeChannelId === id) {
+        setActiveChannelId('1')
+      }
+      showNotification('Канал успешно удален', 'success')
+    } catch (err) {
+      showNotification(`Ошибка удаления канала: ${err.data}`, 'danger')
+    }
+  }
+  return (
+    <>
+      <Dropdown.Menu
+        drop="end"
+        className="w-25"
+        variant="dark"
+        disabled={isLoading}
+      >
+        <Dropdown.Item as="button">Переименовать</Dropdown.Item>
+        <Dropdown.Item as="button" onClick={handleOpen}>
+          Удалить
+        </Dropdown.Item>
+      </Dropdown.Menu>
+
+      {/* Модальное окно для удаления канала */}
+      <ModalWrapper
+        visible={visible}
+        handleClose={handleClose}
+        title="Удалить канал?"
+      >
+        <ModalButtons
+          handleClose={handleClose}
+          isLoading={isLoading}
+          title="Удалить"
+          type="button"
+          onClick={handleRemove}
+        />
+      </ModalWrapper>
+    </>
+  )
+}
