b615ea41ec462df89f29b6338b2ac6a3746afc74
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-19 19:13:59 +0300

    feat: добавить в хук значение возврата если канал не существует -Если канал не найден по id то возвращается канал general и его id становится активным

--- frontend/src/common/hooks/useSelectedChannel.js
+++ frontend/src/common/hooks/useSelectedChannel.js
@@ -1,12 +1,23 @@
 import { useGetChannelsQuery } from '@/features/channels/api/channelsApi'
+import { useContext, useEffect, useMemo } from 'react'
+import { ActiveChannelIdContext } from '@/pages/MainPage/model'
 
 export const useSelectedChannel = (channelId) => {
   const { data: channels = [] } = useGetChannelsQuery()
+  const { setActiveChannelId } = useContext(ActiveChannelIdContext)
+
+  const selectedChannel = useMemo(() => {
+    return channels.find((channel) => channel.id === channelId)
+  }, [channels, channelId])
+
+  //Синхронизируем состояние ID в контексте (срабатывает после рендера)
+  useEffect(() => {
+    // Если selectedChannel не найден (удален или не существует) то активный канал становится general (id: 1)
+    if (channels.length > 0 && !selectedChannel) {
+      setActiveChannelId('1')
+    }
+  }, [channels, selectedChannel, setActiveChannelId])
 
   if (channels.length === 0) return null
-
-  const selectedChannel =
-    channels.find((channel) => channel.id === channelId) || channels[0]
-
-  return selectedChannel
+  return selectedChannel || channels[0]
 }
