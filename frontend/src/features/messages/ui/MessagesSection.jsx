9b57af5efd519024d00f9b36e633edaa63922e67
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-19 19:14:29 +0300

    feat: добавить опциональную цепочку для того чтобы увеличить надежность и предотвратить падения

--- frontend/src/features/messages/ui/MessagesSection.jsx
+++ frontend/src/features/messages/ui/MessagesSection.jsx
@@ -1,6 +1,5 @@
 import MessagesList from './MessagesList'
 import MessageTextarea from './MessageTextarea'
-
 import { useSelectedChannel } from '@/common/hooks'
 
 export const MessagesSection = ({ activeChannelId }) => {
@@ -9,12 +8,12 @@ export const MessagesSection = ({ activeChannelId }) => {
   return (
     <div className="d-flex flex-column w-100 bg-dark-subtle ">
       <div className="p-3 shadow">
-        {selectedChannel && <b>{`# ${selectedChannel.name}`}</b>}
+        {selectedChannel && <b>{`# ${selectedChannel?.name}`}</b>}
       </div>
 
       <div className="flex-grow-1 overflow-hidden">
         <div className="h-100 overflow-auto p-3">
-          <MessagesList channelId={activeChannelId} />
+          <MessagesList channelId={selectedChannel?.id} />
         </div>
       </div>
 
