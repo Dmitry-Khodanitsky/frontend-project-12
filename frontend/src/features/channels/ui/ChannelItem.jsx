520653bb25910424e08a6ccdd56936fa74bf9d5f
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-19 16:30:04 +0300

    feat: добавить контекстное меню для выбора действий с каналом -Добавлена кнопка в блоке с названием канала -При нажатии на кнопку открывается дропдаун с двумя кнопками -Кнопка удалить открывает модальное окно с подтверждением

--- frontend/src/features/channels/ui/ChannelItem.jsx
+++ frontend/src/features/channels/ui/ChannelItem.jsx
@@ -1,14 +1,37 @@
-import { Nav } from 'react-bootstrap'
-const ChannelItem = ({ id, ref, children }) => {
+import { Nav, Dropdown, ButtonGroup, Button } from 'react-bootstrap'
+import { ChannelDropdown } from './ChannelDropdown'
+
+const ChannelItem = ({ channel }) => {
   return (
-    <Nav.Item>
+    <Nav.Item as="li" className="w-100">
       <Nav.Link
-        ref={ref}
-        eventKey={id}
-        className="rounded-0 text-dark-emphasis"
+        eventKey={channel.id}
+        className="d-flex p-1 border-0 rounded-0"
         style={{ '--bs-nav-pills-link-active-bg': '#1a1d20' }}
       >
-        {children}
+        <ButtonGroup className="w-100 border-0">
+          <Button
+            variant="none"
+            className="text-start text-truncate border-0 rounded-0  flex-grow-1"
+          >
+            {`# ${channel.name}`}
+          </Button>
+
+          {channel.removable && (
+            <Dropdown
+              as={ButtonGroup}
+              className="flex-grow-0"
+              onClick={(e) => e.stopPropagation()}
+            >
+              <Dropdown.Toggle
+                split
+                variant="none"
+                className="border-0 rounded-0 text-reset"
+              />
+              <ChannelDropdown id={channel.id} />
+            </Dropdown>
+          )}
+        </ButtonGroup>
       </Nav.Link>
     </Nav.Item>
   )
