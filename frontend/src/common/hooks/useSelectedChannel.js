f3f701ba1339b0fd4a228481372783ce285204a5
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-19 18:51:17 +0300

    feat: добавить guard expression и дефолтный возврат канала general, если по id канал не найден

--- frontend/src/common/hooks/useSelectedChannel.js
+++ frontend/src/common/hooks/useSelectedChannel.js
@@ -2,5 +2,11 @@ import { useGetChannelsQuery } from '@/features/channels/api/channelsApi'
 
 export const useSelectedChannel = (channelId) => {
   const { data: channels = [] } = useGetChannelsQuery()
-  return channels.find((channel) => (channel.id === channelId))
+
+  if (channels.length === 0) return null
+
+  const selectedChannel =
+    channels.find((channel) => channel.id === channelId) || channels[0]
+
+  return selectedChannel
 }
