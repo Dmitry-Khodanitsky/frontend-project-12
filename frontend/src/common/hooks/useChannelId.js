bbe5d79e30deb4afa736d695f55487134f90e845
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-21 18:01:15 +0300

    feat: добавить кастомуный хук для получения активного калана и установки нового активного канала - Устранено дублирование кода в компонентах, за счет использования кастомного селектора

---
+++ frontend/src/common/hooks/useChannelId.js
@@ -0,0 +1,19 @@
+// Хук для получения id активного в данный момент канала и экшен для изменения активного канала
+import { useSelector, useDispatch } from 'react-redux'
+import { currentChannenId, setCurrentChannelId } from '@/app/model/uiSlice'
+
+export const useChannelId = () => {
+  const dispatch = useDispatch()
+
+  return {
+    activeChannelId: useSelector(currentChannenId),
+    setCurrentChannelId: (id) => {
+      dispatch(setCurrentChannelId(id))
+    },
+  }
+}
+
+// Где используется:
+// в AddChannelFrom - при добавиленнии нового канала
+// в ChannelList - для автоматического скролла к ативному в данный момент каналу
+// в useChannelSelector - для обновления установки дефолтного канала, если текущий id не существует
