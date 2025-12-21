3d746cac6445b987e9743601b89b9f28cbb8130d
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-21 18:00:27 +0300

    refactor: заменить Redux диспатч на кастомный хук - Заменено использование Redux useDispatch и экшена setCurrentChannelId  на кастомный хук useChannelId. - Удален импорт useDispatch и setCurrentChannelId из слайса. Добавил импорт useChannelId. Теперь установка активного ID канала происходит через кастомный хук вместо прямого диспатча Redux экшенов.

--- frontend/src/common/hooks/useChannelSelector.js
+++ frontend/src/common/hooks/useChannelSelector.js
@@ -1,26 +1,27 @@
+// хук для получения активного канала по id, если id не существует то возвращается дефолтный канал
+
 import { useGetChannelsQuery } from '@/features/channels/api/channelsApi'
 import { useEffect, useMemo } from 'react'
-import { setCurrentChannelId } from '@/app/model/uiSlice'
-import { useDispatch } from 'react-redux'
+import { useChannelId } from './useChannelId'
 
 export const useChannelSelector = (channelId) => {
   const { data: channels = [] } = useGetChannelsQuery()
-  const dispatch = useDispatch()
+  const { setCurrentChannelId } = useChannelId()
 
   const selectedChannel = useMemo(() => {
     return channels.find((channel) => channel.id === channelId)
   }, [channels, channelId])
 
-  //Синхронизируем состояние ID в контексте (срабатывает после рендера)
   useEffect(() => {
     // Если selectedChannel не найден (удален или не существует) то активный канал становится general (id: 1)
     if (channels.length > 0 && !selectedChannel) {
-      dispatch(setCurrentChannelId('1'))
+      setCurrentChannelId('1')
     }
-  }, [channels, selectedChannel, dispatch])
+  }, [channels, selectedChannel, setCurrentChannelId])
 
   if (channels.length === 0) return null
   return selectedChannel || channels[0]
 }
 
-// используется в messageSection для автоматического переключения на дефолтный канал, если выбранный каланал был удален вместе с сообщениями
+// Где используется:
+// в messageSection для автоматического переключения на дефолтный канал, если выбранный каланал был удален вместе с сообщениями
