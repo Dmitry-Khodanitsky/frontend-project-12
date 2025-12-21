2aefda5ab78c4b4d8f4bbd41bd8bf01088cec1eb
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-21 18:01:31 +0300

    feat: добавить экспорт useChannelId

--- frontend/src/common/hooks/index.js
+++ frontend/src/common/hooks/index.js
@@ -1,3 +1,4 @@
 export { useChannelMessages } from './useChannelMessages'
 export { useScrollTo } from './useScrollTo'
 export { useChannelSelector } from './useChannelSelector'
+export { useChannelId } from './useChannelId'
