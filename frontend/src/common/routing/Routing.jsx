f0b0c853ff95b3a06f0bd66552ebc7fee0598ada
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-21 17:25:57 +0300

    refactor: удалить неиспользуемый контекст

--- frontend/src/common/routing/Routing.jsx
+++ frontend/src/common/routing/Routing.jsx
@@ -1,20 +1,12 @@
 import { Routes, Route } from 'react-router'
 import { Login } from '@/features/auth'
 import { MainPage, NotFoundPage } from '@/pages'
-import { ActiveChannelIdProvider } from '@/pages/MainPage/model'
 
 const Routing = () => {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
-      <Route
-        path="/"
-        element={
-          <ActiveChannelIdProvider>
-            <MainPage />
-          </ActiveChannelIdProvider>
-        }
-      />
+      <Route path="/" element={<MainPage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   )
