1e74c3b666c47030eba23668eb6b60908277faff
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-22 12:35:55 +0300

    refactor: перевести управление модалками удаления и переименования на Redux - Удалено локальное состояние visible и связанные обработчики handleOpen/handleClose - Добавлен импорт useDispatch из react-redux и openModal из uiSlice - Добавлены импорты RenameChannelModal и DeleteChannelModal из ./modals/ - Заменены обработчики кликов на диспатч экшенов openModal с соответствующими типами и channelId - Убрана логика удаления канала и уведомлений из компонента - Удален ModalWrapper и ModalButtons, теперь модалки управляются глобально через Redux - Компоненты DeleteChannelModal и RenameChannelModal теперь рендерятся без пропсов

--- frontend/src/features/channels/ui/ChannelDropdown.jsx
+++ frontend/src/features/channels/ui/ChannelDropdown.jsx
@@ -1,52 +1,32 @@
 import { Dropdown } from 'react-bootstrap'
-import { useState, memo } from 'react'
-import { useRemoveChannelMutation } from '../api/channelsApi'
-import { useNotification } from '@/app/model/NotifyContext'
-import { ModalWrapper, ModalButtons } from '@/common/components'
+import { useDispatch } from 'react-redux'
+import { openModal } from '@/app/model/uiSlice'
+import { memo } from 'react'
+import { RenameChannelModal, DeleteChannelModal } from './modals/'
 
 export const ChannelDropdown = memo(({ id }) => {
-  const [visible, setVisible] = useState(false)
-  const [removeChannel, { isLoading }] = useRemoveChannelMutation()
-  const showNotification = useNotification()
-  const handleOpen = () => setVisible(true)
-  const handleClose = () => setVisible(false)
+  // Состояние модальных окон и обработчики
+  const dispatch = useDispatch()
+  const handleOpenRemoveModal = () =>
+    dispatch(openModal({ type: 'removeChannel', channelId: id }))
+  const handleOpenRenameModal = () =>
+    dispatch(openModal({ type: 'renameChannel', channelId: id }))
 
-  const handleRemove = async () => {
-    try {
-      await removeChannel(id).unwrap()
-      showNotification('Канал успешно удален', 'success')
-    } catch (err) {
-      showNotification(`Ошибка удаления канала: ${err.data}`, 'danger')
-    }
-  }
   return (
     <>
-      <Dropdown.Menu
-        drop="end"
-        className="w-25"
-        variant="dark"
-        disabled={isLoading}
-      >
-        <Dropdown.Item as="button">Переименовать</Dropdown.Item>
-        <Dropdown.Item as="button" onClick={handleOpen}>
+      <Dropdown.Menu drop="end" className="w-25" variant="dark">
+        <Dropdown.Item as="button" onClick={handleOpenRenameModal}>
+          Переименовать
+        </Dropdown.Item>
+        <Dropdown.Item as="button" onClick={handleOpenRemoveModal}>
           Удалить
         </Dropdown.Item>
       </Dropdown.Menu>
 
       {/* Модальное окно для удаления канала */}
-      <ModalWrapper
-        visible={visible}
-        handleClose={handleClose}
-        title="Удалить канал?"
-      >
-        <ModalButtons
-          handleClose={handleClose}
-          isLoading={isLoading}
-          title="Удалить"
-          type="button"
-          onClick={handleRemove}
-        />
-      </ModalWrapper>
+      <DeleteChannelModal />
+      {/* Модальное окно для переименовывания канала  */}
+      <RenameChannelModal />
     </>
   )
 })
