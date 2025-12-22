e0ad80015902e128e38425612ea1d01fe6a0dae1
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-22 12:37:53 +0300

    refactor: перевести управление закрытием модалки на Redux в ModalButtons - Удален пропс handleClose из параметров компонента - Добавлен импорт useDispatch из react-redux и closeModal из uiSlice - Создана новая функция handleClose, которая диспатчит экшен closeModal - Теперь кнопка "Отменить" в модальных окнах управляет закрытием через глобальное состояние Redux

--- frontend/src/common/components/ModalButtons/ModalButtons.jsx
+++ frontend/src/common/components/ModalButtons/ModalButtons.jsx
@@ -1,13 +1,16 @@
 import { Button } from 'react-bootstrap'
 import { LoadingSpinner } from '@/common/components'
+import { useDispatch } from 'react-redux'
+import { closeModal } from '@/app/model/uiSlice'
 
 export const ModalButtons = ({
-  handleClose,
   isLoading,
   title,
   type = 'submit',
   onClick,
 }) => {
+  const dispatch = useDispatch()
+  const handleClose = () => dispatch(closeModal())
   return (
     <div className="d-flex justify-content-end">
       <Button
