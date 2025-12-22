9586cfee7bd73c444866b25068b21a2ec3f74011
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-22 12:36:53 +0300

    feat: добавить хук для получения канала по id

---
+++ frontend/src/common/hooks/useChannelByIdSelector.js
@@ -0,0 +1,9 @@
+import { useGetChannelsQuery } from '@/features/channels/api/channelsApi'
+
+export const useChannelByIdSelector = (id) => {
+  const { data: channels } = useGetChannelsQuery()
+  if (!id || !channels) return null
+  return channels.find((channel) => channel.id === id)
+}
+
+// Используется в RenameChannelModal
