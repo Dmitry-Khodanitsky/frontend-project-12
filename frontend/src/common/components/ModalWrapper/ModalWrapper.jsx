8831f0d8274e61b0673609c6c2fd586618ec2c4f
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-22 12:37:27 +0300

    refactor: перевести управление закрытием модалки на Redux - Удален пропс handleClose из параметров компонента - Добавлен импорт useDispatch из react-redux и closeModal из uiSlice - Создана новая функция handleClose, которая диспатчит экшен closeModal - Теперь закрытие модального окна управляется глобальным состоянием Redux

--- frontend/src/common/components/ModalWrapper/ModalWrapper.jsx
+++ frontend/src/common/components/ModalWrapper/ModalWrapper.jsx
@@ -1,14 +1,16 @@
 import { Modal } from 'react-bootstrap'
+import { useDispatch } from 'react-redux'
+import { closeModal } from '@/app/model/uiSlice'
 
-export const ModalWrapper = ({ visible, handleClose, title, children }) => {
+export const ModalWrapper = ({ visible, title, children }) => {
+  const dispatch = useDispatch()
+  const handleClose = () => dispatch(closeModal())
   return (
     <Modal show={visible} onHide={handleClose}>
       <Modal.Header closeButton>
         <Modal.Title>{title}</Modal.Title>
       </Modal.Header>
-      <Modal.Body>
-        {children}
-      </Modal.Body>
+      <Modal.Body>{children}</Modal.Body>
     </Modal>
   )
 }
