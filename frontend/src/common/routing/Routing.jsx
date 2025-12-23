54c23c3f84948f861d2a3d9e8095c87fba4242c5
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-23 13:03:01 +0300

    refactor: добавить страницы с логином и регистрацией в пути

--- frontend/src/common/routing/Routing.jsx
+++ frontend/src/common/routing/Routing.jsx
@@ -1,12 +1,11 @@
 import { Routes, Route } from 'react-router'
-import { Login, SignUp } from '@/features/auth/ui'
-import { MainPage, NotFoundPage } from '@/pages'
+import { LoginPage, MainPage, NotFoundPage, SignUpPage } from '@/pages'
 
 const Routing = () => {
   return (
     <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<SignUp />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
       <Route path="/" element={<MainPage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
