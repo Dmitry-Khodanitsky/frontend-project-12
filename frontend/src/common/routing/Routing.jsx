f535788ebae499f6d067eaf9149405204f0f96c4
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-23 12:21:26 +0300

    feat: добавить путь для страницы с регистрацией

--- frontend/src/common/routing/Routing.jsx
+++ frontend/src/common/routing/Routing.jsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from 'react-router'
-import { Login } from '@/features/auth'
+import { Login, SignUp } from '@/features/auth/ui'
 import { MainPage, NotFoundPage } from '@/pages'
 
 const Routing = () => {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
       <Route path="/" element={<MainPage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
