db78422d10123b1a8a8a1a6535b14985db80c928
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-24 18:03:19 +0300

    feat: добавить контейнер для уведомлений react-toastify - Добавлен импорт ToastContainer из react-toastify - Добавлен компонент ToastContainer с настройками position="bottom-right", theme="dark", newestOnTop - Подключена инициализация i18n через импорт './app/i18n' - Контейнер для уведомлений теперь рендерится внутри Provider для глобального доступа

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router'
+import { ToastContainer } from 'react-toastify'
 import store from '@/app/model/store'
 import App from '@/app/ui/App'
-import { BrowserRouter } from 'react-router'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './app/i18n'
 
 createRoot(document.getElementById('root')).render(
-  //<StrictMode>
+  // <StrictMode>
   <Provider store={store}>
+    <ToastContainer position="bottom-right" theme="dark" newestOnTop />
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </Provider>
-  //</StrictMode>
+  // </StrictMode>
 )
