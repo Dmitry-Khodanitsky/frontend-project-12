5fcc42716fb3cf649687b4cdebe3bb7d22279431
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-25 17:41:40 +0300

    fix: добавить маскироваку английской ненормативной лексики

--- frontend/src/common/services/profanity.js
+++ frontend/src/common/services/profanity.js
@@ -1,5 +1,6 @@
 import filter from 'leo-profanity'
 
-filter.loadDictionary('ru')
+filter.loadDictionary('en')
+filter.add(filter.getDictionary('ru'))
 
 export const profanityClean = (text) => filter.clean(text)
