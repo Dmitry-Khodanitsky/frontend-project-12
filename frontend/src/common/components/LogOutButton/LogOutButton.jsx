b556de0656f8ef84760c08b4492a6020c3dc9801
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-25 15:19:55 +0300

    refactor: добавить отключение сокета и отчистку кеша при разлогине

--- frontend/src/common/components/LogOutButton/LogOutButton.jsx
+++ frontend/src/common/components/LogOutButton/LogOutButton.jsx
@@ -2,6 +2,8 @@ import { Button } from 'react-bootstrap'
 import { useDispatch } from 'react-redux'
 import { logOut } from '@/features/auth/model/authSlice'
 import { useTranslation } from 'react-i18next'
+import { disconnectSocket } from '@/common/services/socket/socket'
+import { baseApi } from '@/app/api/baseApi'
 
 export const LogOutButton = () => {
   const { t } = useTranslation()
@@ -9,7 +11,10 @@ export const LogOutButton = () => {
   const dispatch = useDispatch()
   const handleLogout = () => {
     dispatch(logOut())
+    dispatch(baseApi.util.resetApiState())
+    disconnectSocket()
   }
+
   return (
     <Button variant="primary" type="button" onClick={handleLogout}>
       {t('common.exit')}
