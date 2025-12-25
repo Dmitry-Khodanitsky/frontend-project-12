9641014c03188846bce941bb064db42c91687a27
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-25 10:44:44 +0300

    chore: добавить rollbar коллектор ошибок

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,14 +7,27 @@ import store from '@/app/model/store'
 import App from '@/app/ui/App'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './app/i18n'
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react'
+
+const rollbarConfig = {
+  accessToken: import.meta.env.VITE_ROLLBAR_TOKEN,
+  environment: import.meta.env.MODE,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+}
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
-  <Provider store={store}>
-    <ToastContainer position="bottom-right" theme="dark" newestOnTop />
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+  <RollbarProvider config={rollbarConfig}>
+    <ErrorBoundary>
+      <Provider store={store}>
+        <ToastContainer position="bottom-right" theme="dark" newestOnTop />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </ErrorBoundary>
+  </RollbarProvider>
+
   // </StrictMode>
 )
