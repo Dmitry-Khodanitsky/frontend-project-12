16d6ed207ac95f2d5813d9c9cec4c973f83710b3
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-12-25 18:44:49 +0300

    refactor: установить канал по дефолту посте отрисовки MainPage

--- frontend/src/pages/MainPage/MainPage.jsx
+++ frontend/src/pages/MainPage/MainPage.jsx
@@ -1,6 +1,11 @@
 import { Navigate } from 'react-router'
 import { selectToken } from '@/features/auth/model/authSlice'
-import { currentChannelId, setCurrentChannelId } from '@/app/model/uiSlice'
+import {
+  selectCurrentChannelId,
+  setCurrentChannelId,
+} from '@/app/model/uiSlice'
+import { useGetChannelsQuery } from '@/features/channels/api/channelsApi'
+import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Tab, Container } from 'react-bootstrap'
 import { ChannelsList } from '@/features/channels/'
@@ -9,7 +14,21 @@ import { MessagesSection } from '@/features/messages/'
 export const MainPage = () => {
   const token = useSelector(selectToken)
   const dispatch = useDispatch()
-  const activeChannelId = useSelector(currentChannelId)
+  const activeChannelId = useSelector(selectCurrentChannelId)
+
+  // Получаем список каналов для установки дефолтного
+  // в параметры передается undefined - потому что эндпоинт getChannels не принимает никаких параметров
+  // в параметры передаем skip: Если это поле равно true, RTK Query не будет выполнять сетевой запрос.
+  const { data: channels } = useGetChannelsQuery(undefined, { skip: !token })
+
+  //Устаналвиваем активный канал после отрисовки
+  useEffect(() => {
+    // Если токен есть, каналы загружены, но активный канал еще не выбран
+    if (channels?.length > 0 && !activeChannelId) {
+      const defaultChannelId = channels[0].id
+      dispatch(setCurrentChannelId(defaultChannelId))
+    }
+  }, [channels, activeChannelId, dispatch])
 
   const handleSelect = (id) => {
     dispatch(setCurrentChannelId(id))
